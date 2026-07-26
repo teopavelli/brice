bd7f24bec2a8cb3e2e5ffac70db1a54038a12af8
teopavelli <teopavelli@proton.me>
2026-07-26 19:47:32 +0300

    Add Food class with spawn method for random cell

---
+++ engine/food.py
@@ -0,0 +1,21 @@
+from helpers.randomizer import random_cell
+
+class Food:
+
+    def spawn(
+
+        self,
+
+        width,
+
+        height
+
+    ):
+
+        return random_cell(
+
+            width,
+
+            height
+
+        )
